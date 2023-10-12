1e4e0e4e340416efa9bf022eeb355550d6ae6672
Fabio <thebloodshadow14@gmail.com>
2023-10-12 15:57:10 +0200

    ADD - getWinner | js

--- js/even-odd.js
+++ js/even-odd.js
@@ -4,7 +4,10 @@ const number = prompt("Insert a number between 1 to 5: ");
 
 const robotNumber = getRobotNumber();
 
-console.log(robotNumber);
+getWinner();
+
+console.log("Player number: ", number);
+console.log("Robot number: ", robotNumber);
 
 function randomNumber(minNumber, maxNumber) {
   return Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
@@ -27,6 +30,30 @@ function getRobotNumber() {
     const tempNumber = randomNumber(1, 5);
     if (tempNumber % 2 === 0) {
       return tempNumber;
+    }
+  }
+}
+
+function getWinner() {
+  if (!number || !robotNumber) {
+    alert("You have to write a number!");
+  } else {
+    if (number + (robotNumber % 2) === 0) {
+      if (number % 2 === 0) {
+        alert("Player won (even)");
+        return;
+      } else {
+        alert("Robot won (even)");
+        return;
+      }
+    } else {
+      if (number % 2 == !0) {
+        alert("Player won (odd)");
+        return;
+      } else {
+        alert("Robot won (odd)");
+        return;
+      }
     }
   }
 }
